9fe3c2cb45b3d8411728dae1d3bbcc70250a260d
winter.you <winter.you@kakaoenterprise.com>
2022-06-04 17:40:00 +0900

    Add Pid generator

--- ui/src/main/java/com/wt/ui/UiApplication.java
+++ ui/src/main/java/com/wt/ui/UiApplication.java
@@ -2,12 +2,15 @@ package com.wt.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class UiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UiApplication.class, args);
+        SpringApplication application = new SpringApplication(UiApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
 }
